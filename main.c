5845dfcc8cdd8bceb65a075a57139d071d922be4
Bruno-Abdo <brunoaabdo@gmail.com>
2024-11-23 01:35:17 -0300

    Alteração de TP1V0 para main

--- main.c
+++ main.c
@@ -5,23 +5,35 @@
 #include <string.h>     // Biblioteca para manipulação de strings
 #include"rules.h"
 
-
-
 int main() {
+
+
+    struct action Jogo;
+    int Colunas = 0;
+    int Linhas = 0;
+    
+    saveFile(&Colunas, &Linhas, Jogo.board);
+
     // Declara a string de entrada com tamanho máximo de 10 caracteres
-    char Move[10] = "UP"; // Inicializa a string com "UP" para Debug
+    //char Move[10] = "UP"; // Inicializa a string com "UP" para Debug
 
     // Usa `gets()` para receber uma nova string de entrada do usuário (Perigoso, mas funcional para testes)
-    gets(Move);
+    //gets(Move);
 
     // Variável para armazenar o próximo movimento
-    int Next = 0;
+    //int Next = 0;
 
     // Loop até que um comando válido seja inserido (Next seja diferente de 0)
-    while (!Next) {
+   /* while (!Next) {
         Next = movesTo(Move); // Chama a função movesTo e armazena o retorno
         printf("%i\n", Next); // Exibe o código correspondente à direção inserida
-    }
+    }*/
+
+    
+
+
+
+
 
     return 0; // Finaliza o programa
 }
